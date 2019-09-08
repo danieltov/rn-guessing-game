d005da70c4be013368f523ba336a51ab4aa0c3ab
Daniel Tovar <daniel@danieltovar.io>
2019-09-08 14:27:37 -0600

    Added image

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,12 +1,23 @@
 import React from 'react'
-import { View, Text, StyleSheet, Button } from 'react-native'
+import { View, StyleSheet, Button, Image } from 'react-native'
+import { BodyText, Title } from '../components'
 
 const GameOverScreen = props => {
   return (
     <View style={styles.screen}>
-      <Text>Game Over!</Text>
-      <Text>The number was: {props.userNum}</Text>
-      <Text>Computer guessed in: {props.guesses} rounds</Text>
+      <Title>Game Over!</Title>
+      <View style={styles.imageContainer}>
+        <Image
+          source={require('../assets/success.jpeg')}
+          // source={{
+          //   uri:
+          //     'https://images.unsplash.com/photo-1551892589-865f69869476?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9'
+          // }}
+          style={styles.image}
+        />
+      </View>
+      <BodyText>The number was: {props.userNum}</BodyText>
+      <BodyText>Computer guessed in: {props.guesses} rounds</BodyText>
       <Button onPress={props.onNewGame} title='Play Again?' />
     </View>
   )
@@ -17,6 +28,17 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 50,
+    overflow: 'hidden',
+    marginVertical: 10
+  },
+  image: {
+    width: '100%',
+    height: '100%'
   }
 })
 
