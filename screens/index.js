45a70e46dc41975afb084c9add1f160aa70290b7
Daniel Tovar <daniel@danieltovar.io>
2019-09-08 11:47:54 -0600

    indexed screens

---
+++ screens/index.js
@@ -0,0 +1,5 @@
+import GameScreen from './GameScreen'
+import GameOverScreen from './GameOverScreen'
+import StartGameScreen from './StartGameScreen'
+
+export { GameOverScreen, GameScreen, StartGameScreen }
