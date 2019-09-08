3df6b1b717e2612bc42e42d3cf79e4191ee88a4e
Daniel Tovar <daniel@danieltovar.io>
2019-09-08 14:57:15 -0600

    finished styling MainButton

--- components/MainButton.js
+++ components/MainButton.js
@@ -4,9 +4,16 @@ import Colors from '../constants/colors'
 
 const MainButton = props => {
   return (
-    <TouchableOpacity onPress={props.onPress}>
-      <View style={{...styles.button, backgroundColor: props.color}}>
-        <Text style={styles.buttonText}>{props.children}</Text>
+    <TouchableOpacity onPress={props.onPress} activeOpacity={0.7}>
+      <View
+        style={{
+          ...styles.button,
+          backgroundColor: props.color,
+          ...props.style
+        }}>
+        <Text style={{ ...styles.buttonText, ...props.textStyles }}>
+          {props.children}
+        </Text>
       </View>
     </TouchableOpacity>
   )
@@ -16,13 +23,14 @@ const styles = StyleSheet.create({
   button: {
     backgroundColor: Colors.primary,
     borderRadius: 10,
-    paddingVertical: 15
+    padding: 12,
+    width: '100%'
   },
   buttonText: {
     color: Colors.light,
     fontFamily: 'operator-mono-bold',
     fontSize: 16,
-    textAlign: 'center',
+    textAlign: 'center'
   }
 })
 
