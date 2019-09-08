cbc014545c899de29252e9c26e6d0f0d681fe21d
Daniel Tovar <daniel@danieltovar.io>
2019-09-08 12:35:03 -0600

    Handling async loading of fonts

--- App.js
+++ App.js
@@ -1,16 +1,35 @@
 import React, { useState } from 'react'
 import { StyleSheet, View } from 'react-native'
+import * as Font from 'expo-font'
+import { AppLoading } from 'expo'
 
 import Header from './components/Header'
 import { StartGameScreen, GameScreen, GameOverScreen } from './screens'
 
+const fetchFonts = () => {
+  Font.loadAsync({
+    'operator-mono': require('./assets/fonts/OperatorMono-Book.otf'),
+    'operator-mono-bold': require('./assets/fonts/OperatorMono-Bold.otf'),
+    'operator-mono-italic': require('./assets/fonts/OperatorMono-BookItalic.otf')
+  })
+}
+
 export default function App() {
   const [userNum, setUserNum] = useState()
   const [guessRounds, setGuessRounds] = useState(0)
+  const [dataLoaded, setDataLoaded] = useState(false)
+
+  if (!dataLoaded)
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setDataLoaded(true)}
+        onError={error => console.log(error)}
+      />
+    )
 
   const startGameHandler = selectedNum => {
     setUserNum(selectedNum)
-    setGuessRounds(0)
   }
   const gameOverHandler = numOfRounds => setGuessRounds(numOfRounds)
 
