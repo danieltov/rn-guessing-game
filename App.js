891d49c69e01d790f4043116cd9f0e2473d712ac
Daniel Tovar <daniel@danieltovar.io>
2019-09-08 11:48:46 -0600

    Built out business logic and rendering logic

--- App.js
+++ App.js
@@ -2,19 +2,25 @@ import React, { useState } from 'react'
 import { StyleSheet, View } from 'react-native'
 
 import Header from './components/Header'
-import StartGameScreen from './screens/StartGameScreen'
-import GameScreen from './screens/GameScreen'
+import { StartGameScreen, GameScreen, GameOverScreen } from './screens'
 
 export default function App() {
   const [userNum, setUserNum] = useState()
+  const [guessRounds, setGuessRounds] = useState(0)
 
-  const startGameHandler = selectedNum => setUserNum(selectedNum)
+  const startGameHandler = selectedNum => {
+    setUserNum(selectedNum)
+    setGuessRounds(0)
+  }
+  const gameOverHandler = numOfRounds => setGuessRounds(numOfRounds)
 
   return (
     <View style={styles.App}>
       <Header title='Guess a Number' />
-      {userNum ? (
-        <GameScreen userChoice={userNum} />
+      {userNum && guessRounds <= 0 ? (
+        <GameScreen userChoice={userNum} onGameOver={gameOverHandler} />
+      ) : guessRounds > 0 ? (
+        <GameOverScreen userNum guesses={guessRounds} />
       ) : (
         <StartGameScreen onStartGame={startGameHandler} />
       )}
